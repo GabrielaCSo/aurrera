966c8474d29efe5a64f8cd3dd93ed9b49d9528d1
alumnog <alumnog@s921-25.itson.edu.mx>
2024-05-07 12:47:56 -0700

    Se agrego otr comentario :0

--- src/main/java/mx/itson/aurrera/entities/iu/Aurrera.java
+++ src/main/java/mx/itson/aurrera/entities/iu/Aurrera.java
@@ -11,6 +11,8 @@ package mx.itson.aurrera.entities.iu;
 public class Aurrera {
     public static void main(String[] args) {
         //Hola esta es una prueba para github, wacamoles
+        
+        //Subido al octogato ;)
     }
     
 }
